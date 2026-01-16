87240b6454b90973966ee15fd87727f133a7a89e
Kelvin Tawiah <kelvintawiah224@gmail.com>
2026-01-16 01:01:42 -0600

    Implement service layer with duration calculation

--- KelvinTawiah.CodingTracker/service/SessionLogService.cs
+++ KelvinTawiah.CodingTracker/service/SessionLogService.cs
@@ -1,22 +1,34 @@
-class SessionLogService
+using KelvinTawiah.CodingTracker.model;
+using KelvinTawiah.CodingTracker.repository;
+
+namespace KelvinTawiah.CodingTracker.service;
+
+public class SessionLogService
 {
+  private readonly SessionLogRepository _repository;
+
+  public SessionLogService(string connectionString)
+  {
+    _repository = new SessionLogRepository(connectionString);
+  }
+
   public void AddLog(SessionLog log)
   {
-
+    _repository.Add(log);
   }
 
   public void ClearLog()
   {
-
+    _repository.Clear();
   }
 
   public List<SessionLog> ViewLogs()
   {
-    throw new NotImplementedException();
+    return _repository.View();
   }
 
-  public List<SessionLog> ViewLogsFor(int Id)
+  public List<SessionLog> ViewLogsFor(int id)
   {
-    throw new NotImplementedException();
+    return _repository.FindBy(id);
   }
 }
--- KelvinTawiah.CodingTracker/service/SessionService.cs
+++ KelvinTawiah.CodingTracker/service/SessionService.cs
@@ -1,27 +1,42 @@
-class SessionService
+using KelvinTawiah.CodingTracker.model;
+using KelvinTawiah.CodingTracker.repository;
+
+namespace KelvinTawiah.CodingTracker.service;
+
+public class SessionService
 {
+  private readonly SessionRepository _repository;
+
+  public SessionService(string connectionString)
+  {
+    _repository = new SessionRepository(connectionString);
+  }
+
   public void AddSession(Session session)
   {
-
+    session.DurationMinutes = (int)(session.EndTime - session.StartTime).TotalMinutes;
+    var id = _repository.Create(session);
+    session.Id = id;
   }
 
-  public Session GetSessionById(int id)
+  public Session? GetSessionById(int id)
   {
-    throw new NotImplementedException();
+    return _repository.FindById(id);
   }
 
-  public void UpdateSession(int id)
+  public void UpdateSession(Session session)
   {
-
+    session.DurationMinutes = (int)(session.EndTime - session.StartTime).TotalMinutes;
+    _repository.Update(session);
   }
 
   public List<Session> GetAllSessions()
   {
-    throw new NotImplementedException();
+    return _repository.GetAll();
   }
 
   public void DeleteSession(int id)
   {
-
+    _repository.Delete(id);
   }
 }
